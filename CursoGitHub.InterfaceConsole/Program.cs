31d345583d109ce8ed8ee519c44b6ad6da9961d2
kellysalesv <fulanodetal@exemplo.br>
2022-07-04 11:14:52 -0300

    iniciando csharp

--- CursoGitHub.InterfaceConsole/Program.cs
+++ CursoGitHub.InterfaceConsole/Program.cs
@@ -11,6 +11,7 @@ namespace CursoGitHub.InterfaceConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Alô Mundo");
+            Console.ReadKey(); 
         }
     }
 }
